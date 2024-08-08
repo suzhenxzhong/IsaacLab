e59aaddd77dbea1b56d72c10db1748c4aef16915
suzhenxzhong <suzhenxzhong@gmail.com>
2024-08-07 23:46:31 -0400

    fix teleop_device , device confliction error

--- source/standalone/environments/teleoperation/teleop_se3_agent.py
+++ source/standalone/environments/teleoperation/teleop_se3_agent.py
@@ -17,7 +17,7 @@ parser.add_argument(
     "--disable_fabric", action="store_true", default=False, help="Disable fabric and use USD I/O operations."
 )
 parser.add_argument("--num_envs", type=int, default=1, help="Number of environments to simulate.")
-parser.add_argument("--device", type=str, default="keyboard", help="Device for interacting with environment")
+parser.add_argument("--teleop_device", type=str, default="keyboard", help="Device for interacting with environment")
 parser.add_argument("--task", type=str, default=None, help="Name of the task.")
 parser.add_argument("--sensitivity", type=float, default=1.0, help="Sensitivity factor.")
 # append AppLauncher cli args
